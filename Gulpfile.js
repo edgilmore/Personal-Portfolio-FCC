0d5d9664a786e19217fb9be641f28d337e709433
Edward Gilmore <edgilmore@live.com>
2015-12-09 20:12:01 -0700

    Configured gulp to compiler boostrap-sass from bower

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,21 +4,46 @@
 'use strict';
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var nodesass = require('node-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var fs = require('fs');
 
 // variables
 var autoprefixerOptions = {
     browsers: ['last 2 versions', '> 5%']
 };
+var config = {
+    filename:{
+        bootstrapcss: 'bootstrap.css'
+    }
+};
+var paths = {
+    bower: './bower_components/',
+    sass: './app/css/sass/',
+    css: './app/css/',
+    js: './app/js/'
+};
 
-gulp.task('sass', function () {
-    return gulp
-        .src('app/css/sass/style.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(gulp.dest('app/css'));
+gulp.task('build:bootstrap', function() {
+    return nodesass
+        .render({file: paths.bower + 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'}, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            return fs.writeFile(paths.css + config.filename.bootstrapcss , result.css.toString());
+        });
 });
 
-gulp.task('default', ['sass'], function () {
+gulp.task('build:sass', function () {
+    return gulp
+        .src(paths.sass + 'style.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(paths.css));
+});
 
+gulp.task('build', ['build:bootstrap', 'build:sass'], function () {
+
+});
+gulp.task('default', function () {
 });
